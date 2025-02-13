8fd2195d3c94bacbeea7bedd7bd78e30748e32bd
marviigrey <marvellousfrederick@gmail.com>
2025-02-13 00:17:40 +0000

    added route table

---
+++ route.tf
@@ -0,0 +1,29 @@
+resource "aws_route_table" "i-one-test_public" {
+    vpc_id = aws_vpc.main.id
+  }
+
+  resource "aws_route_table" "i-one-test_private" {
+    vpc_id = aws_vpc.main.id
+  }
+
+  resource "aws_route_table_association" "i-one-public-a" {
+    subnet_id = aws_subnet.i-one-test-public-zone-a.id
+    route_table_id = aws_route_table.i-one-test_public.id
+  }
+
+  resource "aws_route_table_association" "i-one-public-b" {
+    subnet_id = aws_subnet.i-one-test-public-zone-b.id
+    route_table_id = aws_route_table.i-one-test_public.id
+  }
+  
+  resource "aws_route_table_association" "i-one-private-a" {
+    subnet_id = aws_subnet.i-one-test-private-zone-a.id
+    route_table_id = aws_route_table.i-one-test_private.id
+  }
+
+  resource "aws_route_table_association" "i-one-private-a" {
+    subnet_id = aws_subnet.i-one-test-private-zone-b.id
+    route_table_id = aws_route_table.i-one-test_private.id
+  }
+  
+  
